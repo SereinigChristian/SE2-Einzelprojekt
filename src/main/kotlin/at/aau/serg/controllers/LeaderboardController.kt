045aa7ad3b5313c75390a784ba74e387d43c2618
Christian Sereinig <chsereinig@edu.aau.at>
2026-03-19 11:19:38 +0100

    Implement sorting by score/time and rank parameter logic

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -4,6 +4,7 @@ import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,7 +14,24 @@ class LeaderboardController(
 ) {
 
     @GetMapping
-    fun getLeaderboard(): List<GameResult> =
-        gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, { it.id }))
+    fun getLeaderboard(@RequestParam(required = false) rank: Int?): List<GameResult> {
+        //Sortierung
+        val sortedList = gameResultService.getGameResults()
+            .sortedWith(compareBy({ -it.score }, { it.timeInSeconds }))
 
+        //Aufgabe 2.2.2
+        if (rank == null) return sortedList
+
+        if (rank < 1 || rank > sortedList.size) {
+            throw org.springframework.web.server.ResponseStatusException(
+                org.springframework.http.HttpStatus.BAD_REQUEST, "Ungültiger Rank"
+            )
+        }
+        val index = rank - 1
+        val start = (index - 3).coerceAtLeast(0)
+        val end = (index + 3).coerceAtMost(sortedList.size - 1)
+
+        return sortedList.subList(start, end + 1)
+
+    }
 }
